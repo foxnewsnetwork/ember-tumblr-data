823ad6a48bbb06a4a83ada7fc5063d07f7af5067
Thomas Chen <foxnewsnetwork@gmail.com>
2015-05-18 15:36:51 -0700

    added included callback

--- index.js
+++ index.js
@@ -3,4 +3,8 @@
 
 module.exports = {
   name: 'ember-tumblr-data'
+  included: function(app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + "/js-yaml/dist/js-yaml.js");
+  }
 };
