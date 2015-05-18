8cc3b1a76b4aa0634f0acb743428b0814dc300e4
Thomas Chen <foxnewsnetwork@gmail.com>
2015-05-18 15:37:33 -0700

    forgot comma

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-tumblr-data'
+  name: 'ember-tumblr-data',
   included: function(app) {
     this._super.included(app);
     app.import(app.bowerDirectory + "/js-yaml/dist/js-yaml.js");
